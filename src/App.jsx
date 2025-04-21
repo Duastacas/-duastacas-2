077bd8d43c694963a1e9dacbd6e60d38e04d8f40
Duastacas <duastacasbar@gmail.com>
2025-04-21 18:17:49 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Painel Duas Taças 🥂</h1>
+      <p>Seu sistema está funcionando!</p>
+    </div>
+  );
+}
+
+export default App;
